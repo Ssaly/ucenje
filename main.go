221dafd5dcf92991cf9dc7f23a7e4b2c5ed978e9
Ssaly <salihovic.amar22@gmail.com>
2022-12-01 20:13:03 +0100

    Zad1

--- main.go
+++ main.go
@@ -1,63 +1,86 @@
 package main
 
 import (
+
 	"fmt"
+
 )
 
+func zadatak1() {
+
+	fmt.Println("Exercise 001")
+	
+	res, _ := suma(5, 5)
+	
+	
+	fmt.Println(res)
+	
+	}
+	
+	func suma(a, b int) (int, error) {
+	
+		return a+b, nil 
+	
+	}
+	
+	
 func main() {
 
-	fmt.Println("Welcome to my quiz game!")
+   zadatak1()
 
-	fmt.Printf("Enter your name: ")
-	var name string
-	fmt.Scan(&name)
 
-	fmt.Printf("Hello, %v welcome to the game!\n", name)
+	// fmt.Println("Welcome to my quiz game!")
 
-	fmt.Printf("Enter your age: ")
-	var age uint
-	fmt.Scan(&age)
+	// fmt.Printf("Enter your name: ")
+	// var name string
+	// fmt.Scan(&name)
 
-	if age >= 10 {
+	// fmt.Printf("Hello, %v welcome to the game!\n", name)
 
-		fmt.Println("Welcome to the game! ")
+	// fmt.Printf("Enter your age: ")
+	// var age uint
+	// fmt.Scan(&age)
 
-	} else {
-		fmt.Println("You cannot play!\n")
-		return
-	}
+	// if age >= 10 {
 
-	score := 0
-	num_que := 2
-	fmt.Printf("Which club has won the most Champions League titles? ")
-	var answer string
-	var answer2 string
-	fmt.Scan(&answer, &answer2)
-	fmt.Println(answer, answer2)
-	if answer+" "+answer2 == "Real Madrid" {
-		fmt.Println("Correct!")
-		score += 1
-	} else if answer+" "+answer2 == "real madrid" {
-		fmt.Println("Correct!")
-		score += 1
-	} else if answer+" "+answer2 == "Real madrid" {
-		fmt.Println("Correct!")
-		score += 1
-	} else {
-		fmt.Println("Incorrect!")
-	}
+	// 	fmt.Println("Welcome to the game! ")
 
-	fmt.Printf("Which outfield player appeared in the Champions League final in three different decades? ")
-	fmt.Scan(&answer, &answer2)
-	if answer+" "+answer2 == "Ryan Giggs" || answer+" "+answer2 == "ryan giggs" || answer+" "+answer2 == "ryan gigs" {
-		fmt.Println("Correct!")
-		score += 1
-	} else {
-		fmt.Println("Incorrect!")
-	}
+	// } else {
+	// 	fmt.Println("You cannot play!\n")
+	// 	return
+	// }
 
-	fmt.Printf("You scored %v out of %v.", score, num_que)
-	percent := (float64(score) / float64(num_que)) * 100
+	// score := 0
+	// num_que := 2
+	// fmt.Printf("Which club has won the most Champions League titles? ")
+	// var answer string
+	// var answer2 string
+	// fmt.Scan(&answer, &answer2)
+	// fmt.Println(answer, answer2)
+	// if answer+" "+answer2 == "Real Madrid" {
+	// 	fmt.Println("Correct!")
+	// 	score += 1
+	// } else if answer+" "+answer2 == "real madrid" {
+	// 	fmt.Println("Correct!")
+	// 	score += 1
+	// } else if answer+" "+answer2 == "Real madrid" {
+	// 	fmt.Println("Correct!")
+	// 	score += 1
+	// } else {
+	// 	fmt.Println("Incorrect!")
+	// }
 
-	fmt.Printf("You scored: %v%%.", percent)
+	// fmt.Printf("Which outfield player appeared in the Champions League final in three different decades? ")
+	// fmt.Scan(&answer, &answer2)
+	// if answer+" "+answer2 == "Ryan Giggs" || answer+" "+answer2 == "ryan giggs" || answer+" "+answer2 == "ryan gigs" {
+	// 	fmt.Println("Correct!")
+	// 	score += 1
+	// } else {
+	// 	fmt.Println("Incorrect!")
+	// }
+
+	// fmt.Printf("You scored %v out of %v.", score, num_que)
+	// percent := (float64(score) / float64(num_que)) * 100
+
+	// fmt.Printf("You scored: %v%%.", percent)
 }
---
+++ zadaci/main.go
@@ -0,0 +1,23 @@
+package	main
+
+import (
+	"fmt"
+)
+
+func zadatak1() {
+
+fmt.Println("Exercise 001")
+
+res, _ := suma(5, 5)
+
+
+fmt.Println(res)
+
+}
+
+func suma(a, b int) (int, error) {
+
+	return a+b, nil 
+
+}
+
